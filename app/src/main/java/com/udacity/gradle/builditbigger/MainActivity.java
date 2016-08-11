0fad218c3fe534a445f3fa9d6b4b8a3daeeb542a
Warren Schrader <wwschrader@gmail.com>
2016-08-11 12:02:55 -0700

    Add java library joke provider and have toast display joke from library.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,14 +1,16 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.example.JokeGenerator;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +42,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, JokeGenerator.getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
---
+++ jokeprovider/src/main/java/com/example/JokeGenerator.java
@@ -0,0 +1,7 @@
+package com.example;
+
+public class JokeGenerator {
+    public static String getJoke(){
+        return "What do you call a dog who can do magic? A labracadabrador.";
+    }
+}
